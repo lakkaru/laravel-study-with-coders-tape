a001563b7dfe5a54fe7bbd676da77476d393913f
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-18 05:55:12 +0530

    feat- routes with jast text returns

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function(){
+    return "about";
+});
+Route::get('contact', function(){
+    return "contact";
+});
